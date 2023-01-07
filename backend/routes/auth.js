d70a956a2822f0731a3cb5b6568b39277e13e0df
jay <jayp33233@gmail.com>
2023-01-07 10:12:46 +0530

    jwt added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "JayisagoodB$oy"
 
 
-// create a user using :POST "/api/auth/createuser"
-
-
+// create a user using :POST "/api/auth/createuser" log in not ruquired
 
 router.post('/createuser' ,[
     body('name','Enter a valid name').isLength({ min: 3 }),
@@ -31,11 +32,20 @@ router.post('/createuser' ,[
         email: req.body.email,
         password: securePass                                                                                                                                                                                                                                                                                                                                                                         
       })
-      res.json(user)
+
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      const authToken = jwt.sign(data , JWT_SECRET)
+      // console.log(authToken);
+      res.json({authToken})
+      // res.json(user)
     }
       catch(error){
         console.error(error.message)
-        res.status(500).send("Some ERROR occured")
+        res.status(500).send("Internal SERVER ERROR")
       }
     //   .then(user => res.json(user))
     //   .catch(err=>{console.log(err)
@@ -43,4 +53,41 @@ router.post('/createuser' ,[
     
 })
 
+// create a user using :POST "/api/auth/login" no log in required
+
+router.post('/login' ,[
+  body('email','Enter valid Email').isEmail(),
+  body('password','Minimum Password length 5').exists(),
+], async (req,res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email , password} = req.body;
+  try {
+    let user = await User.findOne({email});
+
+    if (!user) {
+      return res.status(400).json({error:'pleas enter valid credential'})
+      
+    }
+
+    const comparePass = await bcrypt.compare(password,user.password)
+    if (!comparePass) {
+      return res.status(400).json({error:'pleas enter valid credential'})
+    }
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+    const authToken = jwt.sign(data , JWT_SECRET)
+
+    res.json({authToken})
+  } catch(error){
+    console.error(error.message)
+    res.status(500).send("Internal SERVER ERROR ")
+  }
+})
 module.exports = router;
