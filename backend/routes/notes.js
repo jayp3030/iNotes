9a9f124b5442df95e7f592b258ef51593fd796d6
jay <jayp33233@gmail.com>
2023-01-11 17:12:11 +0530

    update notes added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const Note = require('../models/Note')
 const { body, validationResult } = require('express-validator');
 
 
-//rout:1 -  get all notes :GET "/api/auth/fetchallnotes" log ruquired
+//rout:1 -  get all notes :GET "/api/notes/fetchallnotes" log ruquired
 router.get('/fetchallnotes', fetchuser ,async (req,res)=>{
  try {
      const notes = await Note.find({user:req.user.id})
@@ -17,7 +17,7 @@ router.get('/fetchallnotes', fetchuser ,async (req,res)=>{
 
 })
 
-//rout:2 -  add new notes :POST "/api/auth/addnote" log ruquired
+//rout:2 -  add new notes :POST "/api/notes/addnote" log ruquired
 router.post('/addnote', fetchuser,[
     body('title','Enter a valid title').isLength({ min: 3 }),
     body('description','Description must be minimum length 5').isLength({ min: 5 }),
@@ -38,6 +38,40 @@ router.post('/addnote', fetchuser,[
         console.error(error.message)
         res.status(500).send("Internal SERVER ERROR ")
     }
+})
+
+//rout:3 -  UPDATE notes :POST "/api/notes/updatenote" log ruquired
+
+router.put('/updatenote/:id', fetchuser,async (req,res)=>{
+    const {title , description , tag} = req.body;
+
+    const newNote ={}
+
+    if (title) {
+        newNote.title=title
+    }
+    if (description) {
+        newNote.description=description
+    }
+    if (tag) {
+        newNote.tag=tag
+    }
+
+    // find a note to be updated and update it 
+    let note = await Note.findById(req.params.id)
+
+    if (!note) {
+       return res.status(404).send('NOT FOUND')
+    }
+
+    if (String(note.user) !== req.user.id) {
+        console.log('im running')
+        return res.status(401).send('Not Allowed')
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id , {$set:newNote} , {new:true})
+
+    res.json({note})
 })
 
 module.exports = router;
