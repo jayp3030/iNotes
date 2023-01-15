8c95f2fad235d521860dec578ff59bb838efb64f
jay <jayp33233@gmail.com>
2023-01-15 11:26:16 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter , Route,Routes } from 'react-router-dom';
-import './App.css';
-import About from './components/About';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import NoteState from "./contexts/notes/NoteState";
 
 function App() {
   return (
-    <div className="App">
-       <BrowserRouter>
-       <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/about' element={<About/>}/>
-        </Routes>
-      </BrowserRouter> 
-    </div>
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
+    </>
   );
 }
 
---
+++ src/contexts/notes/NoteState.js
@@ -0,0 +1,11 @@
+import NoteContext from "./noteContext";
+
+const NoteState =(props)=>{
+    return (
+        <NoteContext.Provider>
+            {props.children}
+        </NoteContext.Provider>
+    )
+} 
+
+export default NoteState();
